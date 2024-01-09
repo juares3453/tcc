7075d16cd91c51425d5f5deacbadbc85800ac0d4
Juares <juarescanalle@gmail.com>
2024-01-09 07:42:20 -0300

    iniciando web parte 1

--- app.py
+++ app.py
@@ -1,49 +1,50 @@
-import pyodbc
-import numpy as np # linear algebra
-import pandas as pd # data manipulation and analysis
-import matplotlib.pyplot as plt # data visualization
-import seaborn as sns # data visualization
-from time import time
-from sklearn import metrics
-from sklearn.cluster import KMeans
-from sklearn import preprocessing
-from sklearn.preprocessing import scale
-from sklearn.decomposition import PCA
-from sqlalchemy import create_engine
+#import pyodbc
+#import numpy as np # linear algebra
+#import pandas as pd # data manipulation and analysis
+#import matplotlib.pyplot as plt # data visualization
+#import seaborn as sns # data visualization
+#from time import time
+#from sklearn import metrics
+#from sklearn.cluster import KMeans
+#from sklearn import preprocessing
+#from sklearn.preprocessing import scale
+#from sklearn.decomposition import PCA
+#from sqlalchemy import create_engine
 
-from flask import Flask, render_template
-app = Flask(__name__)
+#from flask import Flask, render_template
+#app = Flask(__name__)
+
 
 # Informações da conexão
-server = '10.0.0.14'  # Exemplo: 'localhost'
-database = 'softran_rasador'  
-username = 'softran'  
-password = 'sof1209'  
+#server = '10.0.0.14'  # Exemplo: 'localhost'
+#database = 'softran_rasador'  
+#username = 'softran'  
+#password = 'sof1209'  
 
 # String de conexão usando SQLAlchemy
-connection_str = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver=SQL+Server'
+#connection_str = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver=SQL+Server'
 
 # Criando o motor de conexão
-engine = create_engine(connection_str)
+#engine = create_engine(connection_str)
 
 # Comandos SQL
 # Função para ler o conteúdo SQL do arquivo
-def ler_sql_do_arquivo(nome_do_arquivo):
-    with open(nome_do_arquivo, 'r') as arquivo:
-        return arquivo.read()
+#def ler_sql_do_arquivo(nome_do_arquivo):
+#    with open(nome_do_arquivo, 'r') as arquivo:
+#        return arquivo.read()
 
 # Lendo os comandos SQL dos arquivos
-sql_comando1 = ler_sql_do_arquivo("C:\\Users\\juare\\Desktop\\TCC\\Dados TCC.sql")
-sql_comando2 = ler_sql_do_arquivo("C:\\Users\\juare\\Desktop\\TCC\\Dados TCC Plus.sql")
+#sql_comando1 = ler_sql_do_arquivo("C:\\Users\\juare\\Desktop\\TCC\\Dados TCC.sql")
+#sql_comando2 = ler_sql_do_arquivo("C:\\Users\\juare\\Desktop\\TCC\\Dados TCC Plus.sql")
 
 # Executando os comandos e lendo os dados para DataFrames
-df1 = pd.read_sql(sql_comando1, engine)
-df2 = pd.read_sql(sql_comando2, engine)
-df1_copia = pd.read_sql(sql_comando1, engine)
-df2_copia = pd.read_sql(sql_comando2, engine)
+#df1 = pd.read_sql(sql_comando1, engine)
+#df2 = pd.read_sql(sql_comando2, engine)
+#df1_copia = pd.read_sql(sql_comando1, engine)
+#df2_copia = pd.read_sql(sql_comando2, engine)
 
 # Fechando a conexão
-engine.dispose()
+#engine.dispose()
 
 # Exibindo os primeiros registros para verificar
 # print("Resultado 1:")
@@ -51,100 +52,100 @@ engine.dispose()
 # print("\nResultado 2:")
 # print(df2.head())
 
-def index_of_dic(dic, key):
-    return dic[key]
+#def index_of_dic(dic, key):
+#    return dic[key]
 
-def StrList_to_UniqueIndexList(lista):
-    group = set(lista)
-    print(group)
+#def StrList_to_UniqueIndexList(lista):
+#    group = set(lista)
+#    print(group)
 
-    dic = {}
-    i = 0
-    for g in group:
-        if g not in dic:
-            dic[g] = i
-            i += 1
+ #   dic = {}
+ #   i = 0
+ #   for g in group:
+ #       if g not in dic:
+ #           dic[g] = i
+ #           i += 1
 
-    print(dic)
-    return [index_of_dic(dic, p) for p in lista]
+  #  print(dic)
+  #  return [index_of_dic(dic, p) for p in lista]
 
 # Supondo que 'df1' é o seu DataFrame
-df1['Filial'] = StrList_to_UniqueIndexList(df1['Filial'])
+#df1['Filial'] = StrList_to_UniqueIndexList(df1['Filial'])
 
-# Exibindo as primeiras linhas dos DataFrames
-print("\nDados (Colunas):")
-print(df1.columns)
-print("Dados (original):")
-print(df1_copia.head(5))  
-print("\nDados (modificado):")
-print(df1.head(5))
-print("\nDados (Infos Variáveis):")
-print(df1.info())
-print("\nDados (Shape):")
-print(df1.shape)
-print("\nDados (Describe):")
-print(df1.describe())
-print("\nDados (Describe = include 0):")
-print(df1.describe(include='O'))
+# Exibindo as primeiras linhas dos DataFrames#
+#print("\nDados (Colunas):")
+#print(df1.columns)
+#print("Dados (original):")
+#print(df1_copia.head(5))  
+#print("\nDados (modificado):")
+#print(df1.head(5))
+#print("\nDados (Infos Variáveis):")
+#print(df1.info())
+#print("\nDados (Shape):")
+#print(df1.shape)
+#print("\nDados (Describe):")
+#print(df1.describe())
+#print("\nDados (Describe = include 0):")
+#print(df1.describe(include='O'))
 
 #Data Cleaning
-print("\nDados (Limpeza):")
-print(df1.isnull().sum())
+#print("\nDados (Limpeza):")
+#print(df1.isnull().sum())
 
 #Data outliers
-df1[df1.duplicated(keep='first')]
-df1.drop_duplicates(keep='first',inplace=True)
+#df1[df1.duplicated(keep='first')]
+#df1.drop_duplicates(keep='first',inplace=True)
 
 #Filial versus conferencia de carregamento
 #Charges
-plt.figure(figsize=(10,6))
-sns.distplot(df1.Filial,color='r')
-plt.title('Distribuição Filial - Carregamento',size=18)
-plt.xlabel('Filial',size=14)
-plt.ylabel('conf_carregamento',size=14)
-plt.show()
+#plt.figure(figsize=(10,6))
+#sns.distplot(df1.Filial,color='r')
+#plt.title('Distribuição Filial - Carregamento',size=18)
+#plt.xlabel('Filial',size=14)
+#plt.ylabel('conf_carregamento',size=14)
+#plt.show()
 
 #Age
-plt.figure(figsize=(10,6))
-sns.histplot(df1.Filial)
-plt.title('Distribuição Filial - Carregamento',size=18)
-plt.xlabel('Filial',size=14)
-plt.ylabel('conf_carregamento',size=14)
-plt.show()
+#plt.figure(figsize=(10,6))
+#sns.histplot(df1.Filial)
+#plt.title('Distribuição Filial - Carregamento',size=18)
+#plt.xlabel('Filial',size=14)
+#plt.ylabel('conf_carregamento',size=14)
+#plt.show()
 
 #BMI
-plt.figure(figsize=(10,6))
-plt.hist(df1.Filial,color='y')
-plt.title('Distribuição Filial',size=18)
-plt.show()
+#plt.figure(figsize=(10,6))
+#plt.hist(df1.Filial,color='y')
+#plt.title('Distribuição Filial',size=18)
+#plt.show()
 
 #BMI
-plt.figure(figsize=(10,6))
-plt.hist(df1.conf_carregamento,color='y')
-plt.title('Distribuição conf_carregamento',size=18)
-plt.show()
+#plt.figure(figsize=(10,6))
+#plt.hist(df1.conf_carregamento,color='y')
+#plt.title('Distribuição conf_carregamento',size=18)
+#plt.show()
 
 #Boxplot
-plt.figure(figsize = (10,6))
-sns.boxplot(df1.Filial)
-plt.title('Distribution Filial',size=18)
-plt.show()
-Q1 = df1['Filial'].quantile(0.25)
-Q3 = df1['Filial'].quantile(0.75)
-IQR = Q3 - Q1
-print(IQR)
-df1[(df1['Filial']< Q1-1.5* IQR) | (df1['Filial']> Q3+1.5* IQR)]
+#plt.figure(figsize = (10,6))
+#sns.boxplot(df1.Filial)
+#plt.title('Distribution Filial',size=18)
+#plt.show()
+#Q1 = df1['Filial'].quantile(0.25)
+#Q3 = df1['Filial'].quantile(0.75)
+#IQR = Q3 - Q1
+#print(IQR)
+#df1[(df1['Filial']< Q1-1.5* IQR) | (df1['Filial']> Q3+1.5* IQR)]
 
 #Boxplot
-plt.figure(figsize = (10,6))
-sns.boxplot(df1.conf_carregamento)
-plt.title('Distribution conf_carregamento',size=18)
-plt.show()
-Q1 = df1['conf_carregamento'].quantile(0.25)
-Q3 = df1['conf_carregamento'].quantile(0.75)
-IQR = Q3 - Q1
-print(IQR)
-df1[(df1['conf_carregamento']< Q1-1.5* IQR) | (df1['conf_carregamento']> Q3+1.5* IQR)]
+#plt.figure(figsize = (10,6))
+#sns.boxplot(df1.conf_carregamento)
+#plt.title('Distribution conf_carregamento',size=18)
+#plt.show()
+#Q1 = df1['conf_carregamento'].quantile(0.25)
+#Q3 = df1['conf_carregamento'].quantile(0.75)
+#IQR = Q3 - Q1
+#print(IQR)
+#df1[(df1['conf_carregamento']< Q1-1.5* IQR) | (df1['conf_carregamento']> Q3+1.5* IQR)]
 
 #Gender
 plt.figure(figsize=(10,6))
--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_file
 import pandas as pd
+from io import StringIO
 from sqlalchemy import create_engine
 import seaborn as sns
 from io import BytesIO
@@ -16,9 +17,9 @@ graficos_dir = 'static/graficos'
 os.makedirs(graficos_dir, exist_ok=True)
 
 # Configuração da conexão com o banco de dados
-server = '10.0.0.14'
+server = 'JUARES-PC'
 database = 'softran_rasador'
-username = 'softran'
+username = 'sa'
 password = 'sof1209'
 connection_str = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver=SQL+Server'
 engine = create_engine(connection_str)
@@ -36,21 +37,83 @@ sql_comando1 = ler_sql_do_arquivo("C:\\Users\\juare\\Desktop\\TCC\\Dados TCC Plu
 def get_dataframe(sql_comando):
     with engine.connect() as conn:
         df = pd.read_sql(sql_comando, conn)
+
+    def index_of_dic(dic, key):
+        return dic[key]
+
+    def StrList_to_UniqueIndexList(lista):
+        group = set(lista)
+        print(group)
+
+        dic = {}
+        i = 0
+        for g in group:
+            if g not in dic:
+                dic[g] = i
+                i += 1
+
+        print(dic)
+        return [index_of_dic(dic, p) for p in lista]
+
+    # Supondo que 'df1' é o seu DataFrame
+    df['Filial'] = StrList_to_UniqueIndexList(df['Filial'])
     return df
 
 def get_dataframe1(sql_comando1):
     with engine.connect() as conn:
         df1 = pd.read_sql(sql_comando1, conn)
-    return df
+    return df1
 
 def gerar_grafico(df, tipo_grafico, nome_arquivo):
     plt.figure()  # Cria uma nova figura
-    if tipo_grafico == 'histograma':
-        sns.histplot(df['Filial'])  # Exemplo
-    elif tipo_grafico == 'boxplot':
-        sns.boxplot(x='Filial', y='conf_carregamento', data=df)  # Exemplo
-    # Adicione mais condições conforme necessário
 
+    df['conf_carregamento'] = pd.to_numeric(df['conf_carregamento'], errors='coerce')
+
+    if tipo_grafico == 'histograma':
+        plt.figure(figsize=(10,6))
+        sns.histplot(df.Filial)
+        plt.title('Distribuição Filial - Carregamento',size=18)
+        plt.xlabel('Filial',size=14)
+        plt.ylabel('conf_carregamento',size=14)
+    elif tipo_grafico == 'boxplot':
+        plt.figure(figsize=(10,6))
+        sns.boxplot(x='Filial', y='conf_carregamento', data=df)  
+        plt.title('Distribuição Filial - Carregamento',size=18)
+        plt.xlabel('Filial',size=14)
+        plt.ylabel('conf_carregamento',size=14)
+    elif tipo_grafico == 'displot':
+        plt.figure(figsize=(10,6))
+        sns.distplot(df.Filial,color='r')
+        plt.title('Distribuição Filial - Carregamento',size=18)
+        plt.xlabel('Filial',size=14)
+        plt.ylabel('conf_carregamento',size=14)
+    elif tipo_grafico == 'hist':
+        plt.figure(figsize=(10,6))
+        plt.hist(df.Filial,color='y')
+        plt.title('Distribuição Filial',size=18)   
+    elif tipo_grafico == 'hist_car':    
+        plt.figure(figsize=(10,6))
+        plt.hist(df.conf_carregamento,color='y')
+        plt.title('Distribuição Conf_carregamento',size=18) 
+    elif tipo_grafico == 'box_iqr':   
+        plt.figure(figsize = (10,6))
+        sns.boxplot(df.Filial)
+        plt.title('Distribution Filial',size=18)
+        Q1 = df['Filial'].quantile(0.25)
+        Q3 = df['Filial'].quantile(0.75)
+        IQR = Q3 - Q1
+        plt.ylabel(IQR,size=14)
+        df[(df['Filial']< Q1-1.5* IQR) | (df['Filial']> Q3+1.5* IQR)]
+    elif tipo_grafico == 'box_iqr_carre':   
+        plt.figure(figsize = (10,6))
+        sns.boxplot(df.conf_carregamento)
+        plt.title('Distribution conf_carregamento',size=18)
+        Q1 = df['conf_carregamento'].quantile(0.25)
+        Q3 = df['conf_carregamento'].quantile(0.75)
+        IQR = Q3 - Q1
+        plt.ylabel(IQR,size=14)
+        df[(df['conf_carregamento']< Q1-1.5* IQR) | (df['conf_carregamento']> Q3+1.5* IQR)]
+        
     caminho_arquivo = os.path.join('static', 'graficos', f'{nome_arquivo}.png')
     plt.savefig(caminho_arquivo)
     plt.close()
@@ -64,16 +127,37 @@ def index():
 # Rota para visualizar o dashboard
 @teste.route('/dashboard')
 def dashboard():
-    sql_comando = ler_sql_do_arquivo("C:\\Users\\juare\\Desktop\\TCC\\Dados TCC.sql")
     df = get_dataframe(sql_comando)
+
+    #Data outliers
+    df[df.duplicated(keep='first')]
+    df.drop_duplicates(keep='first',inplace=True)
+
+    # Captura a saída de df.info()
+    buffer = StringIO()
+    df.info(buf=buffer)
+    infos_variaveis = buffer.getvalue()
+
+    dados_texto = {
+        'colunas': df.columns.tolist(),
+        'dados_originais': df.head(5).to_html(classes='table'),
+        'infos_variaveis': infos_variaveis,
+        'shape': df.shape,
+        'describe': df.describe().to_html(classes='table'),
+        'limpeza': df.isnull().sum()
+    }
 
     graficos = {
         'histograma': gerar_grafico(df, 'histograma', 'histograma_filial'),
-        'boxplot': gerar_grafico(df, 'boxplot', 'boxplot_filial')
-        # Adicione mais gráficos conforme necessário
+        'boxplot': gerar_grafico(df, 'boxplot', 'boxplot_filial'),
+        'displot': gerar_grafico(df, 'displot', 'displot_filial'),
+        'hist': gerar_grafico(df, 'hist', 'hist_filial'),
+        'hist_car': gerar_grafico(df, 'hist_car', 'hist_carregamento'),
+        'box_iqr': gerar_grafico(df, 'box_iqr', 'box_filial'),
+        'box_iqr_carre': gerar_grafico(df, 'box_iqr_carre', 'box_carre')
     }
 
-    return render_template('dashboard.html', graficos=graficos)
+    return render_template('dashboard.html', graficos=graficos, dados_texto=dados_texto)
 
 if __name__ == '__main__':
     teste.run(debug=True)
