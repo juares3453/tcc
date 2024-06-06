88b2c63894cf861fe1d161b1ffaef282c87ad5c3
juares3453 <juarescanalle@gmail.com>
2024-06-06 14:03:08 +0000

    começando tratamento de dados cenario 2

--- tres1.py
+++ tres1.py
@@ -4,7 +4,6 @@ import os
 from io import StringIO
 import itertools
 from tqdm import tqdm
-import pandas as pd
 import seaborn as sb
 from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
 from sklearn.decomposition import PCA
@@ -18,6 +17,7 @@ from sklearn.model_selection import train_test_split, cross_val_predict
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.impute import SimpleImputer
 
+import pandas as pd
 mpl.use('Agg')
 mpl.rcParams['figure.max_open_warning'] = 50
 analise = Flask(__name__)
@@ -32,86 +32,78 @@ campos2 = ['Resp', 'CLIENTE', 'dtcte','mescte','anocte','dtemissao','mesemissao'
 
 csv_filepath2 = os.path.join('df2.csv')
 
-# Função para remover valores negativos
-def remover_valores_negativos(df):
-    for coluna in df.columns:
-        if pd.api.types.is_numeric_dtype(df[coluna]):
-            df[coluna] = df[coluna].apply(lambda x: x if x >= 0 else np.nan)
-    return df
-
 # Função para carregar e preparar os dados
 def get_dataframe(filepath):
-    df = pd.read_csv(filepath, encoding='cp1252', delimiter=';')
-    df = remover_valores_negativos(df)
-    return df
+    df2 = pd.read_csv(filepath, encoding='cp1252', delimiter=';')
+    return df2
 
 @analise.route('/gerar_graficos')
 def gerar_graficos():
-    csv_filepath = os.path.join('df2.csv')
-    df = get_dataframe(csv_filepath)
+    csv_filepath2 = os.path.join('df2.csv')
+    df2 = get_dataframe(csv_filepath2)
 
     # Substituição de vírgulas por pontos na coluna 'VlCusto'
-    df['VlCusto'] = df['VlCusto'].str.replace(',', '.')
+    df2['VlCusto'] = df2['VlCusto'].str.replace(',', '.')
 
     # Conversão de tipos de dados
-    df['VlCusto'] = pd.to_numeric(df['VlCusto'], errors='coerce')
+    df2['VlCusto'] = pd.to_numeric(df2['VlCusto'], errors='coerce')
 
     # Tratamento de datas - preenchendo valores ausentes temporariamente com valores válidos
-    df[['dtbaixa', 'mesbaixa', 'anobaixa']] = df[['dtbaixa', 'mesbaixa', 'anobaixa']].fillna(0)
+    df2[['dtbaixa', 'mesbaixa', 'anobaixa']] = df2[['dtbaixa', 'mesbaixa', 'anobaixa']].fillna(0)
 
     # Conversão de colunas para inteiros
-    df[['dtbaixa', 'mesbaixa', 'anobaixa']] = df[['dtbaixa', 'mesbaixa', 'anobaixa']].astype(int)
+    df2[['dtbaixa', 'mesbaixa', 'anobaixa']] = df2[['dtbaixa', 'mesbaixa', 'anobaixa']].astype(int)
 
     # Codificação de variáveis categóricas
     label_encoder = LabelEncoder()
-    df['tp_ocor'] = label_encoder.fit_transform(df['tp_ocor'])
-    df['Situacao'] = label_encoder.fit_transform(df['Situacao'])
-    df['dsocorrencia'] = label_encoder.fit_transform(df['dsocorrencia'])
-    df['DsLocal'] = label_encoder.fit_transform(df['DsLocal'])
-    df['CLIENTE'] = label_encoder.fit_transform(df['CLIENTE'])
+    df2['tp_ocor'] = label_encoder.fit_transform(df2['tp_ocor'])
+    df2['Situacao'] = label_encoder.fit_transform(df2['Situacao'])
+    df2['dsocorrencia'] = label_encoder.fit_transform(df2['dsocorrencia'])
+    df2['DsLocal'] = label_encoder.fit_transform(df2['DsLocal'])
+    df2['CLIENTE'] = label_encoder.fit_transform(df2['CLIENTE'])
 
      # Combinação de colunas de dia, mês e ano em colunas de datas completas
-    df['data_cte'] = pd.to_datetime(df[['anocte', 'mescte', 'dtcte']].astype(str).agg('-'.join, axis=1), errors='coerce')
-    df['data_emissao_bo'] = pd.to_datetime(df[['anoemissao', 'mesemissao', 'dtemissao']].astype(str).agg('-'.join, axis=1), errors='coerce')
-    df['data_ocor'] = pd.to_datetime(df[['anoocor', 'mesocor', 'dtocor']].astype(str).agg('-'.join, axis=1), errors='coerce')
-    df['data_baixa'] = pd.to_datetime(df[['anobaixa', 'mesbaixa', 'dtbaixa']].astype(str).agg('-'.join, axis=1), errors='coerce')
+    df2['data_cte'] = pd.to_datetime(df2[['anocte', 'mescte', 'dtcte']].astype(str).agg('-'.join, axis=1), errors='coerce')
+    df2['data_emissao_bo'] = pd.to_datetime(df2[['anoemissao', 'mesemissao', 'dtemissao']].astype(str).agg('-'.join, axis=1), errors='coerce')
+    df2['data_ocor'] = pd.to_datetime(df2[['anoocor', 'mesocor', 'dtocor']].astype(str).agg('-'.join, axis=1), errors='coerce')
+    df2['data_baixa'] = pd.to_datetime(df2[['anobaixa', 'mesbaixa', 'dtbaixa']].astype(str).agg('-'.join, axis=1), errors='coerce')
 
     # Substituição de NaT por uma data padrão
-    df['data_baixa'].fillna(pd.Timestamp('1900-01-01'), inplace=True)
+    df2['data_baixa'].fillna(pd.Timestamp('1900-01-01'), inplace=True)
     
     # Exclusão de colunas de dia, mês e ano originais, se necessário
-    df = df.drop(columns=['dtcte', 'mescte', 'anocte', 'dtemissao', 'mesemissao', 'anoemissao', 'dtocor', 'mesocor', 'anoocor', 'dtbaixa', 'mesbaixa', 'anobaixa'])
+    df2 = df2.drop(columns=['dtcte', 'mescte', 'anocte', 'dtemissao', 'mesemissao', 'anoemissao', 'dtocor', 'mesocor', 'anoocor', 'dtbaixa', 'mesbaixa', 'anobaixa', 'data_cte', 'data_emissao_bo', 'data_ocor', 'data_baixa'])
 
     # Tratamento de valores nulos
     imputer = SimpleImputer(strategy='mean')
-    df[['diasresolucao', 'DsLocal', 'diasemissao']] = imputer.fit_transform(df[['diasresolucao', 'DsLocal', 'diasemissao']])
+    df2[['diasresolucao', 'DsLocal', 'diasemissao']] = imputer.fit_transform(df2[['diasresolucao', 'DsLocal', 'diasemissao']])
 
     # Remoção de duplicatas
-    df.drop_duplicates(inplace=True)
+    df2.drop_duplicates(inplace=True)
 
     # Normalização dos dados
     scaler = MinMaxScaler()
     colunas_para_normalizar = ['diasresolucao', 'diasemissao', 'NrBo', 'dsocorrencia', 'CLIENTE', 'VlCusto', 'DsLocal', 'Resp']
-    df[colunas_para_normalizar] = scaler.fit_transform(df[colunas_para_normalizar])
+    df2[colunas_para_normalizar] = scaler.fit_transform(df2[colunas_para_normalizar])
 
     # Análise e tratamento de outliers
-    Q1 = df['VlCusto'].quantile(0.25)
-    Q3 = df['VlCusto'].quantile(0.75)
+    Q1 = df2['VlCusto'].quantile(0.25)
+    Q3 = df2['VlCusto'].quantile(0.75)
     IQR = Q3 - Q1
-    df = df[~((df['VlCusto'] < (Q1 - 1.5 * IQR)) | (df['VlCusto'] > (Q3 + 1.5 * IQR)))]
+    df2 = df2[~((df2['VlCusto'] < (Q1 - 1.5 * IQR)) | (df2['VlCusto'] > (Q3 + 1.5 * IQR)))]
 
     # Impressão das informações no console
     print("Shape do DataFrame:")
-    print(df.shape)
+    print(df2.shape)
     print(" ")
     print("Valores nulos por coluna:")
-    print(df.isnull().sum())
+    print(df2.isnull().sum())
     print(" ")
     print("Tipos de dados:")
-    print(df.dtypes)
+    print(df2.dtypes)
     print(" ")
     print("Primeiras linhas do DataFrame:")
-    print(df.head())
+    print(df2.head())
 
     return "Processamento concluído e informações exibidas no console."
 
